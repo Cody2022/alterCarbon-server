83fce393fdfe8cf9cdec5aced215c52cccfdcb4a
Siri <sirilakku01@gmail.com>
2022-04-13 19:07:35 -0600

    updates new versions

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
-const express = require('express');
-const res = require('express/lib/response');
+const projectRoutes = require("./routes/projectRoutes");
+const express = require("express");
 const app = express();
-const {welcome,inputs} = require('./functions')
 
-let userName
-let electricity
-let naturalGas
-let carMiles
-let plasticWaste
+app.use(express.json())
+app.use("/api",projectRoutes);
 
-app.get("/", welcome)
-
-app.post("/userInfo", (req,res)=>{
-  //res.sendFile('./index.html', {'root':"C:/Programming/alterCarbon"});
-})
 
 const PORT = 4000;
 function echoPortNumber() {
   console.log(`Listening on port ${PORT}`);
 }
 app.listen(PORT, echoPortNumber);
-
