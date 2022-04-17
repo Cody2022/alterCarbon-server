71a71394582947cc476b2e4f48a954e5bb8a0c66
Siri <sirilakku01@gmail.com>
2022-04-16 18:49:06 -0600

    Formated file

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const { append } = require('express/lib/response');
+const express = require("express");
+const { append } = require("express/lib/response");
 const router = express.Router();
-const { welcomePage, loadUserState, createUserState} = require ("./../model/functions.js")
+const {
+  welcomePage,
+  loadUserState,
+  createUserState,
+} = require("./../model/functions.js");
 
-const { carbonCalculation } = require('../model/carbonCalculation.js');
-const bodyParser=require('body-parser')
-const bcrypt = require('bcrypt');
+const { carbonCalculation } = require("../model/carbonCalculation.js");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 
-router.use(bodyParser.json())
+router.use(bodyParser.json());
 
-
-router.get("/welcome", (req,res) =>{
-    let startMessage = welcomePage()
-    res.send(startMessage)
-} )
+router.get("/welcome", (req, res) => {
+  let startMessage = welcomePage();
+  res.send(startMessage);
+});
 
 // router.get("/userInfo", async(req,res) =>{
 //     let userId = req.query.userId
@@ -27,8 +30,6 @@ router.get("/welcome", (req,res) =>{
 //     res.send(responseMessage)
 // } )
 
-
-
 // router.post("/createUser", async(req,res) =>{
 //     console.log(req.body)
 //     // let userId = req.query.userId
@@ -39,37 +40,34 @@ router.get("/welcome", (req,res) =>{
 
 //*** Cody added the following section*/
 router.post("/login", (req, res) => {
-    console.log((user = req.body.username));
-    console.log((password = req.body.password));
-    // connect with DB to check passwword and then send token to client
-    if (password === "123456") {
-      res.send({
-        token: 'successful',
-      });
-    }else {
-      res.send({Message: "Wrong email or password"})
-    };
-   
-  });
+  console.log((user = req.body.username));
+  console.log((password = req.body.password));
+  // connect with DB to check passwword and then send token to client
+  if (password === "123456") {
+    res.send({
+      token: "successful",
+    });
+  } else {
+    res.send({ Message: "Wrong email or password" });
+  }
+});
 
+router.post("/carbon", async (req, res) => {
+  const carbonEmission = await carbonCalculation(req.body);
+  res.send(carbonEmission);
+});
 
-router.post("/carbon", async (req,res)=>{
-    const carbonEmission=await carbonCalculation(req.body)
-    res.send(carbonEmission)
-})
+router.post("/register", async (req, res) => {
+  console.log("req.body is", req.body);
+  const { username, password } = req.body;
+  //if email is not found in the database, hash password and save account to database
+  //else return "email" has been registered
 
-router.post("/register", async (req,res)=>{
-    console.log("req.body is", req.body)
-    const {username,password}=req.body;
-    //if email is not found in the database, hash password and save account to database
-    //else return "email" has been registered
-    
-    const hash=await bcrypt.hash(password, saltRounds=10);
-    //Save hash to database
-    res.json(hash);
-})
+  const hash = await bcrypt.hash(password, (saltRounds = 10));
+  //Save hash to database
+  res.json(hash);
+});
 
 /* ---------*/
 
-
-module.exports = router
+module.exports = router;
