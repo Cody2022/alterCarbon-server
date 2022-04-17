7abe31ea3ca72a276ffa5f3420440badbf992d06
Siri <sirilakku01@gmail.com>
2022-04-16 18:45:23 -0600

    Allowing only read operation on test()

--- model/testMongoose.js
+++ model/testMongoose.js
@@ -2,16 +2,16 @@ const { createUser, findUserById, updateUserById, deleteUserById } = require("./
 
 
 async function test(){
-    // let createdUser = await createUser({userName : "Zack",electricity : 0,naturalGas :0,carMiles :0,plasticWaste :0})
-    // console.log(createdUser)
+    //  let createdUser = await createUser({userName : "ABC",electricity : 0,naturalGas :0,carMiles :0,plasticWaste :0})
+    //  console.log(createdUser)
 
-    // let findId = await findUserById("62588cd8d4d34ef40dfbb8d0")
-    // console.log(findId)
+    let findId = await findUserById("62588cd8d4d34ef40dfbb8d0")
+    console.log(findId)
 
     // let updateUser = await updateUserById ("62588cd8d4d34ef40dfbb8d0",{userName:'Hari'})
 
-    let deletedUser = await deleteUserById ("625784e997756eb29f836a25")
-    console.log(deletedUser)
+    // let deletedUser = await deleteUserById ("625784e997756eb29f836a25")
+    // console.log(deletedUser)
 
 
 }
