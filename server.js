d6ad16ac46181d1adb2623505a04b366b7d355b4
Cody2022 <cody.zheng@hotmail.com>
2022-09-28 23:28:39 -0600

    Cody_added public folder for deployment

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const projectRoutes = require("./routes/projectRoutes");
 const express = require("express");
 const app = express();
+const path=require('path')
 
 app.use(express.json())
 app.use("/api",projectRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 const PORT = 4000;
